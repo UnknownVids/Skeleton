bb716467ca82c1373607d6658470c944f86bad0d
Mohammed Murshed <P2600414@my365.dmu.ac.uk>
2022-04-15 21:46:50 +0100

    Week 23 Page 31

--- ClassLibrary/clsScreen.cs
+++ ClassLibrary/clsScreen.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 
 /// <summary>
 /// Manages the screening rooms that are available at a venue, including the creation of new rooms, removal of old ones and the 
@@ -81,29 +82,44 @@ namespace ClassLibrary
         //private data member for the Screen ID
         private Int32 mScreenID;
         //ScreenID
-        public Int32 ScreenID 
+        public int ScreenID
         {
             get
             {
                 //this line of the code sends data out of the screen
                 return mScreenID;
-                }
+            }
             set
             {
                 //this line of the code allows data into the screen
                 mScreenID = value;
             }
-            }
-        public bool Find(int screenID)
+        }
+        public bool Find(int ScreenID)
         {
-            //set the private data members to the test data value
-            mScreenID = 21;
-            mScreenName = "IMAX1";
-            mScreenBeingUsed = true;
-            mCapacity = 20;
-            mAdsBeforeMovie = 15;
-            //always return true
-            return true;
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the paramter for the ScreenID to search for
+            DB.AddParameter("@ScreenID", ScreenID);
+            //execute the store procedure
+            DB.Execute("sproc_tblScreen_FilterByScreenID");
+            //if one of the record is found (there should be either one or zero!)
+            if (DB.Count == 1)
+            {
+                mScreenID = Convert.ToInt32(DB.DataTable.Rows[0]["ScreenID"]);
+                mScreenName = Convert.ToString(DB.DataTable.Rows[0]["ScreenName"]);
+                mScreenBeingUsed = Convert.ToBoolean(DB.DataTable.Rows[0]["ScreenBeingUsed"]);
+                mCapacity = Convert.ToInt32(DB.DataTable.Rows[0]["Capacity"]);
+                mAdsBeforeMovie = Convert.ToInt32(DB.DataTable.Rows[0]["AdsBeforeMovie"]);
+                //always return true
+                return true;
+            }
+            //if no record is found
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
         }
     }
 }
